45cd3adf5ed04042a3bd3f6d88ffaf6961e25cd0
shootiettv <shootiettv@gmail.com>
2025-11-18 21:01:38 -0700

    fixing start.bat

--- start.bat
+++ start.bat
@@ -1,30 +1,20 @@
 @echo off
-echo ===============================
-echo UTEP Professor Ranking Starter (Windows)
-echo ===============================
+echo UTEP Professor Ranking Starter
+echo Killing old uvicorn and vite processes...
 
-echo Killing old uvicorn and Vite processes...
-
-:: Kill uvicorn and vite processes if they exist
 taskkill /F /IM uvicorn.exe >nul 2>&1
 taskkill /F /IM node.exe >nul 2>&1
 
-:: Free ports 8000 and 5173
-echo Checking for processes on ports 8000 and 5173...
-echo test
-
-:: -------- Kill port 5173 --------
+echo checking for processes on port 5173
 for /f "tokens=5" %%B in ('netstat -ano ^| findstr ":5173"') do (
-    echo Freeing port 5173 (PID %%B)...
+    echo Freeing port 5173 (PID %%B)
     taskkill /F /PID %%B >nul 2>&1
 )
 
-:: Move to script directory
 cd /d "%~dp0"
 
 echo Starting FastAPI backend...
 start cmd /k "python -m uvicorn src.main:app --reload --host 127.0.0.1 --port 8000"
 
-echo  Starting React frontend...
+echo Starting react frontend...
 npm run dev
-echo All services started.
